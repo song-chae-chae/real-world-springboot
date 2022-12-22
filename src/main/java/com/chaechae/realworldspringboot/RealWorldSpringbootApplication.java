7fb3531843017a3b095500424c603baa3d3afe2a
chaechae <songchaechaes@gmail.com>
2022-12-22 21:36:43 +0900

    project commit

---
+++ src/main/java/com/chaechae/realworldspringboot/RealWorldSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.chaechae.realworldspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealWorldSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealWorldSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chaechae/realworldspringboot/RealWorldSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaechae.realworldspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealWorldSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
