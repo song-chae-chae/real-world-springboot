3877f357f2968269e55e33e7013d3906ca5aeb71
chaechae <songchaechaes@gmail.com>
2022-12-29 20:11:41 +0900

    add BaseEntity to add common created, modified time field

---
+++ src/main/java/com/chaechae/realworldspringboot/base/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.chaechae.realworldspringboot.base;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(updatable = true)
+    private LocalDateTime modifiedAt;
+}
