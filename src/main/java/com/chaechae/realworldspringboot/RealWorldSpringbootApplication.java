6a71da9595d19a0650985be80c466667626ebadf
chaechae <songchaechaes@gmail.com>
2022-12-30 23:29:24 +0900

    add EnableJpaAuditing annotation

--- src/main/java/com/chaechae/realworldspringboot/RealWorldSpringbootApplication.java
+++ src/main/java/com/chaechae/realworldspringboot/RealWorldSpringbootApplication.java
@@ -2,10 +2,11 @@ package com.chaechae.realworldspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class RealWorldSpringbootApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RealWorldSpringbootApplication.class, args);
 	}
