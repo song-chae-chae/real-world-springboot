6dae24caa1b9cfaa1b09ccbddb5249937c54adba
chaechae <songchaechaes@gmail.com>
2023-01-03 22:32:58 +0900

    modified RestTemplateExceptionHandler

--- src/main/java/com/chaechae/realworldspringboot/base/exception/handler/RestTemplateExceptionHandler.java
+++ src/main/java/com/chaechae/realworldspringboot/base/exception/handler/RestTemplateExceptionHandler.java
@@ -3,6 +3,7 @@ package com.chaechae.realworldspringboot.base.exception.handler;
 import com.chaechae.realworldspringboot.base.exception.RestTemplateException;
 import com.chaechae.realworldspringboot.base.exception.RestTemplateExceptionType;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.client.ClientHttpResponse;
@@ -14,23 +15,23 @@ import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 @Slf4j
+@RequiredArgsConstructor
 public class RestTemplateExceptionHandler implements ResponseErrorHandler {
+    private final ObjectMapper objectMapper;
 
     @Override
     public boolean hasError(ClientHttpResponse response) throws IOException {
-        return true;
+        return response.getStatusCode().isError();
     }
 
     @Override
     public void handleError(ClientHttpResponse response) throws IOException {
     }
 
-    //
     @Override
     public void handleError(URI url, HttpMethod method, ClientHttpResponse response) throws IOException {
         String responseBody = new String(response.getBody().readAllBytes(), StandardCharsets.UTF_8);
-        ObjectMapper objectMapper = new ObjectMapper();
-        Map<String, String> map = objectMapper.readValue(responseBody, Map.class);
+        Map<String, Object> map = objectMapper.readValue(responseBody, Map.class);
 
         throw new RestTemplateException(RestTemplateExceptionType.INVALID_REST_API_CALL, map);
     }
