86ea0c4d10400f2d09c4035422b2cea5cf47bb7f
chaechae <songchaechaes@gmail.com>
2023-01-03 22:34:00 +0900

    add exception handler at RestTemplate bean and ObjectMapper bean

--- src/main/java/com/chaechae/realworldspringboot/config/ApplicationConfig.java
+++ src/main/java/com/chaechae/realworldspringboot/config/ApplicationConfig.java
@@ -1,6 +1,7 @@
 package com.chaechae.realworldspringboot.config;
 
 import com.chaechae.realworldspringboot.base.exception.handler.RestTemplateExceptionHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +12,12 @@ public class ApplicationConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplateBuilder()
-                .errorHandler(new RestTemplateExceptionHandler())
+                .errorHandler(new RestTemplateExceptionHandler(objectMapper()))
                 .build();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
     }
 }
