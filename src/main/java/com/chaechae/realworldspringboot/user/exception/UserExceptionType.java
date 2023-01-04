41de98bcb99c63ca7eaa8c573dfd476dbb4429c7
chaechae <songchaechaes@gmail.com>
2023-01-04 11:17:19 +0900

    modified UserExceptionType enum

--- src/main/java/com/chaechae/realworldspringboot/user/exception/UserExceptionType.java
+++ src/main/java/com/chaechae/realworldspringboot/user/exception/UserExceptionType.java
@@ -2,15 +2,13 @@ package com.chaechae.realworldspringboot.user.exception;
 
 import com.chaechae.realworldspringboot.base.exception.RealWorldExceptionType;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 @Getter
-@RequiredArgsConstructor
 public enum UserExceptionType implements RealWorldExceptionType {
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "회원을 찾을 수 없습니다.");
 
-    private HttpStatus status;
+    private final HttpStatus status;
     private final String message;
 
     UserExceptionType(HttpStatus status, String message) {
