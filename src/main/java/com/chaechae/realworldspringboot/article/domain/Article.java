5661348046e60bf76dfe1ad40d829d61ec716d86
chaechae <songchaechaes@gmail.com>
2023-01-09 14:48:46 +0900

    modified Article to add join user and add update method

--- src/main/java/com/chaechae/realworldspringboot/article/domain/Article.java
+++ src/main/java/com/chaechae/realworldspringboot/article/domain/Article.java
@@ -1,10 +1,13 @@
 package com.chaechae.realworldspringboot.article.domain;
 
+import com.chaechae.realworldspringboot.article.request.ArticleUpdate;
+import com.chaechae.realworldspringboot.user.domain.User;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -20,14 +23,36 @@ public class Article {
     private String description;
 
     @OneToMany(mappedBy = "article")
-    private Set<Tag> tags;
+    private Set<Tag> tags = new HashSet<>();
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
     @Builder
-    public Article(Long id, String title, String content, String description, Set<Tag> tags) {
+    public Article(Long id, String title, String content, String description, Set<Tag> tags, User user) {
         this.id = id;
         this.title = title;
         this.content = content;
         this.description = description;
-        this.tags = tags;
+        this.user = user;
+    }
+
+    public void update(ArticleUpdate request) {
+        this.title = request.getTitle();
+        this.content = request.getContent();
+        this.description = request.getDescription();
+        this.tags = request.getTags() == null ? this.tags : request.getTags();
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", description='" + description + '\'' +
+                ", user=" + user +
+                '}';
     }
 }
