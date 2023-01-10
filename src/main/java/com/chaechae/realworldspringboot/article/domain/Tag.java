d92adb014c8735488d2457f2217b6fa1ae36728a
chaechae <songchaechaes@gmail.com>
2023-01-10 14:59:22 +0900

    feat: Tag entity extends BaseEntity

--- src/main/java/com/chaechae/realworldspringboot/article/domain/Tag.java
+++ src/main/java/com/chaechae/realworldspringboot/article/domain/Tag.java
@@ -1,5 +1,6 @@
 package com.chaechae.realworldspringboot.article.domain;
 
+import com.chaechae.realworldspringboot.base.BaseEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Entity
 @Getter
 @NoArgsConstructor
-public class Tag {
+public class Tag extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
