23cfe6de5ab7bf54faed1f86aea78038ce721d62
chaechae <songchaechaes@gmail.com>
2023-01-16 23:04:48 +0900

    fix: UserResponse에 id 추가

--- src/main/java/com/chaechae/realworldspringboot/user/response/UserResponse.java
+++ src/main/java/com/chaechae/realworldspringboot/user/response/UserResponse.java
@@ -6,11 +6,13 @@ import lombok.Getter;
 
 @Getter
 public class UserResponse {
+    private final Long id;
     private final String email;
     private final String username;
     private final String image;
     @Builder
     public UserResponse(User user) {
+        this.id = user.getId();
         this.email = user.getEmail();
         this.username = user.getName();
         this.image = user.getImage();
