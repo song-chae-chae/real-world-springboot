dbe6c53fb907e44842b0d7dd431e29a2fc585ac3
chaechae <songchaechaes@gmail.com>
2023-01-29 16:41:23 +0900

    feat: 초기 데이터 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,18 @@
+INSERT INTO `users` (name, email, image, role, social_id, created_at) VALUES('회원1', 'user1@gmail.com', 'https://cdn-icons-png.flaticon.com/512/3177/3177440.png', 'USER', '125124', '2022-11-20 17:00:40')
+
+INSERT INTO `users` (name, email, image, role, social_id, created_at) VALUES('회원2', 'user2@gmail.com', 'https://cdn-icons-png.flaticon.com/512/7175/7175408.png', 'USER', '345423', '2022-11-24 17:00:40')
+
+INSERT INTO `users` (name, email, image, role, social_id, created_at) VALUES('회원3', 'user3@gmail.com', 'https://cdn-icons-png.flaticon.com/512/3870/3870822.png', 'USER', '623123', '2023-01-10 17:00:40')
+
+INSERT INTO `article` (content, description, title, user_id, created_at, modified_at) VALUES('게시판 인증 구현했음', '인증', '게시판 만들기 1', 1, '2022-11-25 17:00:40', '2022-11-25 17:00:40')
+INSERT INTO `article` (content, description, title, user_id, created_at, modified_at) VALUES('게시판에 글쓰기 했음', '글쓰기', '게시판 만들기 2', 1, '2022-11-26 19:00:40', '2022-11-26 19:00:40')
+INSERT INTO `article` (content, description, title, user_id, created_at, modified_at) VALUES('게시판에 댓글도 달았음', '댓글', '게시판 만들기 3', 1, '2023-01-20 17:00:40', '2023-01-20 17:00:40')
+
+INSERT INTO `article` (content, description, title, user_id, created_at, modified_at) VALUES('갈비탕에 밥 말아먹기', '갈비탕', '점심 메뉴 추천 1', 2, '2022-12-25 17:00:40', '2022-12-25 17:00:40')
+INSERT INTO `article` (content, description, title, user_id, created_at, modified_at) VALUES('닭갈비 먹고 밥도 볶아먹기', '닭갈비', '점심 메뉴 추천 2', 2, '2023-01-14 17:00:40', '2023-01-14 17:00:40')
+
+INSERT INTO `article` (content, description, title, user_id, created_at, modified_at) VALUES('앙드레 브라질리에 보고옴', '앙드레', '전시회 갔다온 후기 푼다 1', 3, '2023-01-10 17:00:40', '2023-01-10 17:00:40')
+INSERT INTO `article` (content, description, title, user_id, created_at, modified_at) VALUES('인터스텔라', '영화', '블랙홀의 이해', 3, '2023-01-13 17:00:40', '2023-01-13 17:00:40')
+INSERT INTO `article` (content, description, title, user_id, created_at, modified_at) VALUES('에릭 요한슨 사진전도 보고옴', '사진전', '전시회 갔다온 후기 푼다 2', 3, '2023-01-15 17:00:40', '2023-01-15 17:00:40')
+INSERT INTO `article` (content, description, title, user_id, created_at, modified_at) VALUES('승요하고 싶었는데 패요됨', '직관', '야구장 갔다 온 후기 1', 3, '2023-01-19 17:00:40', '2023-01-19 17:00:40')
+
